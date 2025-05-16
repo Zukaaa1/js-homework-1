dbea28d994264036d90504619dc87ec1338a4c74
Zurii488 <119165662+Zurii488@users.noreply.github.com>
2025-05-16 18:33:47 +0400

    Adding Variables

---
+++ script.js
@@ -0,0 +1,16 @@
+// Zura 5/16/2025
+// ?Create variables
+let firstName = "Zura";
+let lastName = "Japaridze";
+let email = "zurajaparidze4@gmail.com";
+let age = 19;
+let hobby = "Djing";
+let isStudent;
+
+// ! Update some values
+hobby = "Basketball";
+age = 20;
+
+// !Print updated values
+console.log(hobby);
+console.log(age);
